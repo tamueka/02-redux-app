afe7b1a64371b2e24f41aa22cd3775c2fdec9105
= <=>
2022-02-14 21:15:57 +0100

    instalamos @ngrx/store-devtools + instalamos extension navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment'; // Angular CLI environment
+
 
 // NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { contadorReducer } from './contador/contador.reducer';
 
 import { AppComponent } from './app.component';
@@ -11,7 +14,14 @@ import { NietoComponent } from './contador/nieto/nieto.component';
 
 @NgModule({
   declarations: [AppComponent, HijoComponent, NietoComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ contador: contadorReducer })],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
